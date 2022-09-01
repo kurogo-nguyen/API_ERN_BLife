c7ad1c3d274b09e736982897be1760ece41ac3a8
kurogo-nguyen <52783990+kurogo-nguyen@users.noreply.github.com>
2022-09-01 14:49:30 +0700

    add start up event, parameter and logic function

--- main.py
+++ main.py
@@ -1,50 +1,44 @@
+import csv
+from typing import Union
+
 import mne.filter
 import numpy as np
 from fastapi import FastAPI
-from pydantic import BaseModel
+from mne.baseline import rescale
+from mne.decoding import Vectorizer
 
+from model import SVM, EegChunk, ET
 
 # =============
 # === Model ===
 # =============
 
-
-class Channel(BaseModel):
-    name: str
-    data: list[float]
-
-
-class EEGSignal(BaseModel):
-    timestamp: int = 0
-    ET: bool = 0
-    # sample_rate: int = 128
-    # length: int = 128
-    eeg_signal: list[float]
-
-    #
-    # @validator("eeg_signal")
-    # def parse_values(cls, value):
-    #     return np.array(value, dtype=float)
-
-    # class Config:
-    #     arbitrary_types_allowed = True
-
-
 # =================
 # === Parameter ===
 # =================
+
 
 SAMPLING_RATE = 128
 L_FREQ = 0.16
 H_FREQ = 30.
 
-BUFFER_SIZE = 256
-SEGMENT_TIME = 800  # milliseconds
-PICKS = ['Fz', 'Cz']
-DOWN_SAMPLE = 8
+BUFFER_TIME = 4   # seconds
+SEGMENT_TIME = 1  # seconds
 
-eeg_data = np.zeros((32, 1))
-current_time_stamp = 0
+BUFFER_SIZE = BUFFER_TIME * SAMPLING_RATE
+SEGMENT_SIZE = SEGMENT_TIME * SAMPLING_RATE+1
+# SEGMENT_SIZE = 0
+PICK_IDX = []
+DOWN_SAMPLE = 1
+
+
+PREDICT_COUNT = 3
+
+eeg_buffer = []
+timestamp_buffer = []
+
+# current_time_stamp = 0
+svm_model = None
 
 
 # times = 1
@@ -54,30 +48,28 @@ current_time_stamp = 0
 # ================
 
 
-def update_data(eeg_signal, timestamp):
+def update_buffer(chunk):
     """
     Update EEG buffer
 
     Parameters
     ----------
-    eeg_signal : array_like
-        Values of all sensor in a frame.
-    timestamp : int
-        Timestamp of frame.
+    chunk: EegChunk
+        chunk of eeg signal receive from emotiv
 
     Returns
     -------
-    None
+
     """
-    global eeg_data
-    global current_time_stamp
+    global eeg_buffer
+    global timestamp_buffer
 
-    sampling_length = len(eeg_data[0])
-    eeg_data = np.insert(eeg_data, len(eeg_data[0]), np.array(eeg_signal[3:-2]), axis=1)
-    current_time_stamp = timestamp
+    # sampling_length = len(eeg_buffer[0])
+    eeg_buffer.extend(chunk.eeg_data)
+    timestamp_buffer.extend(chunk.timestamp)
 
-    if sampling_length > BUFFER_SIZE:
-        eeg_data = np.delete(eeg_data, 0, axis=1)
+    while len(eeg_buffer) > BUFFER_SIZE:
+        eeg_buffer.pop()
 
     return
 
@@ -86,6 +78,7 @@ def processing(raw_eeg):
     """
     Processing eeg data
 
+    - reformat
     - filter
     - remove noise
     - smooth
@@ -94,43 +87,67 @@ def processing(raw_eeg):
 
     Parameters
     ----------
-    raw_eeg : np.ndarray
+    raw_eeg : list
         EEG signal buffer.
 
     Returns
     -------
     segment: np.ndarray
         EEG segment for detect ERN.
-
-
     """
-    filtered = mne.filter.filter_data(raw_eeg,
+    global timestamp_buffer, PICK_IDX
+    eeg = np.array(raw_eeg).T
+
+    picked = eeg[PICK_IDX]
+
+    filtered = mne.filter.filter_data(picked,
                                       sfreq=SAMPLING_RATE,
-                                      l_freq=L_FREQ, h_freq=H_FREQ,)
+                                      l_freq=L_FREQ, h_freq=H_FREQ,
+                                      method='iir',
+                                      verbose=0)
 
     resampled = mne.filter.resample(filtered,
-                                    down=DOWN_SAMPLE)
+                                    down=DOWN_SAMPLE,
+                                    verbose=0)
+    # mne.filter.s
+    # segmented = resampled[:, -1024:]
 
-    segmented = resampled[:, -SEGMENT_TIME*SAMPLING_RATE/DOWN_SAMPLE:]
+    segmented = resampled[:, -SEGMENT_SIZE:]
 
-    return segmented
+    rescaled = rescale(data=segmented, times=np.array(timestamp_buffer[-SEGMENT_SIZE:])-timestamp_buffer[-SEGMENT_SIZE],
+                       baseline=(0, 0.4),  mode='mean')
+
+    vect = Vectorizer()
+
+    trans = vect.fit_transform([rescaled])
+
+    return trans
 
 
-def predict(eeg_signal):
-    """
-    Predict eeg data have ERN or non ERN
-
-    :param eeg_signal:
-    :return: ERN or not ERN (bool)
-    """
-    svm = load('saved_model/svm3.joblib')
-
-    result = svm.predict(eeg_signal)
-
-    return result[0]
+def is_correct(eeg_signal):
+    return svm_model.detect_ern(eeg_signal)
 
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    global svm_model, L_FREQ, H_FREQ, PICK_IDX
+
+    svm_model = SVM()
+    svm_model.load_model()
+
+    L_FREQ = svm_model.freq[0]
+    H_FREQ = svm_model.freq[1]
+
+    f = open('channels.csv')
+    file = csv.reader(f, delimiter=',')
+    channels = []
+    for row in file:
+        channels.append(row[1])
+    # PICKS = svm_model.channels
+    PICK_IDX = mne.pick_channels(ch_names= channels, include=svm_model.channels)
 
 
 # ==============
@@ -143,15 +160,14 @@ async def base():
     return "Home"
 
 
-@app.put("/")
-async def check_ern(frame: EEGSignal):
-    hasERN = False
-    update_data(frame.eeg_signal, frame.timestamp)
-    if frame.ET:
-        segment_data = processing(eeg_data)
-        if predict(segment_data):
-            hasERN = True
-
-        return {"Has ERN": f"{hasERN}"}
+@app.put("/test")
+async def check_error_action(chunk: EegChunk, et: Union[ET, None] = None):
+    update_buffer(chunk)
+    if et is not None:
+        if et.is_clicked:
+            segment_data = processing(eeg_buffer)
+            if not is_correct(segment_data):
+                return {"error": 1}
+        return {"error": 0}
     else:
-        return {"Has ERN": 0}
+        return {"error": 0}
